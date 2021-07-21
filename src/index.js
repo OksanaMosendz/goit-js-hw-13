c93c9c87ec2d68dd5e54ed6dc1bab1c7c3a5b2d3
OksanaMosendz <bagheera03@gmail.com>
2021-07-22 01:40:21 +0300

    install lib

--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
 import './sass/main.scss';
+import photoCard from './templates/photo-card.hbs'
+import { Notify, Report, Confirm, Loading, Block } from "notiflix";
+import SimpleLightbox from "simplelightbox";
+const axios = require('axios').default;
