35b28a7fd253842d7a8c1ac728032417d80d0d68
OksanaMosendz <bagheera03@gmail.com>
2021-07-23 01:54:16 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,34 @@
 import './sass/main.scss';
 import photoCard from './templates/photo-card.hbs'
-import { Notify, Report, Confirm, Loading, Block } from "notiflix";
+import Notiflix from "notiflix";
 import SimpleLightbox from "simplelightbox";
 const axios = require('axios').default;
+
+const input=document.querySelector('#search-form');
+console.log(input);
+
+const API={
+  url:'https://pixabay.com/api/',
+  key:'22611406-1fc8dc647f338efc8b1d9d866',
+  q: 'dog',
+  image_type: 'photo',
+  orientation: 'horizontal',
+  safesearch: true,
+  page: 1,
+  per_page: 40,
+  createURLtoFetch(){return `${this.url}?key=${this.key}&q=${this.q}&image_type=${this.image_type}&orientation=${this.orientation}&safesearch=${this.safesearch}&page=${this.page}&per_page=${this.per_page}`
+},}
+
+
+fetch(API.createURLtoFetch())
+  .then(response => response.json())
+  .then(cards =>{
+    console.log(cards.hits);
+    if(cards.hits.length===0){
+      Notiflix.Notify.failure( "Sorry, there are no images matching your search query. Please try again.")
+    }
+    console.log(cards);
+  }
+  )
+  .catch(error => console.log(error));
+
